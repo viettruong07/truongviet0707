a024ed44694a9c7d94270dd942a9b259323057da
Trương Công Việt <111971657+viettruong07@users.noreply.github.com>
2023-03-03 09:29:13 +0700

    Update TestFlight.java

--- src/BaiThi/TestFlight.java
+++ src/BaiThi/TestFlight.java
@@ -3,6 +3,8 @@ package BaiThi;
 public class TestFlight {
     public static void main(String[] args) {
         Flight g1 = new Flight(857, "Toronto");
+        System.out.println(g1.getNumber());
+        System.out.println(g1.getDestination());
         g1.display();
     }
 }
